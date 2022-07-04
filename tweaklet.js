ce6d39b48e55eafb2b3adfe6272efa872b3e1d47
Eric Ros <ekros@outlook.com>
2022-07-04 14:35:47 +0200

    Make elements selectable, so actions are applied in them. Add Delete action.

--- tweaklet.js
+++ tweaklet.js
@@ -5,6 +5,7 @@
 let target;
 let scale = 1;
 let enabled = true;
+let selectedElement;
 const inlineElements = ["A", "ABBR", "ACRONYM", "B", "BDI", "BDO", "BIG", "BR", "BUTTON", "CANVAS", "CITE", "CODE", "DATA", "DATALIST", "DEL", "DFN", "DEL", "DFN", "EM", "EMBED", "I", "IFRAME", "IMG", "INPUT", "INS", "KBD", "LABEL", "MAP", "MARK", "METER", "NOSCRIPT", "OBJECT", "OUTPUT", "PICTURE", "PROGRESS", "Q", "RUBY", "S", "SAMP", "SCRIPT", "SELECT", "SLOT", "SMALL", "SPAN", "STRONG", "SUB", "SUP", "SVG", "TEMPLATE", "TEXTAREA", "TIME", "U", "TT", "VAR", "VIDEO", "WBR"];
 const actionHistory = [];
 
@@ -19,56 +20,59 @@ const handleMouseMove = ev => {
     } else {
       target.style.transform += ` translate(${ev.clientX-target.originalLeft}px, ${ev.clientY-target.originalTop}px)`;
     }
-    console.log("target.style.transform", target.style.transform)
   }
 }
 
 const handleMouseUp = ev => {
-  console.log("mouseup")
   document.removeEventListener("mouseup", handleMouseUp);  
   document.removeEventListener("mousemove", handleMouseMove); 
   if (target) {
    target.style.filter = "none"; 
   }
-  console.log("target", target, "x", ev.target.originalLeft, "y", ev.target.originalTop);
-  console.log("left", ev.clientX, "top", ev.clientY)
   actionHistory.push({
     target,
     action: "move",
     value: { left: ev.clientX, top: ev.clientY }
   });
-  console.log("actionHistory", actionHistory);
   target = null;
 }
 
 const handleMouseDown = ev => {
-  ev.stopPropagation();
-  console.log("ev.target", ev.target.style);
-    console.log("target", ev.target, "x", ev.target.originalLeft, "y", ev.target.originalTop);
-  target = ev.target;
-  target.style.filter = "blur(1px) grayscale(100%)";
-  document.addEventListener("mousemove", handleMouseMove);
-  document.addEventListener("mouseup", handleMouseUp);
-  actionHistory.push({
-    target,
-    action: "move",
-    value: { left: ev.target.originalLeft, top: ev.target.originalTop }
-  });
+  if (ev.button === 0) {
+    ev.stopPropagation();
+    if (selectedElement) {
+      selectedElement.style.outline = "none";
+    }
+    selectedElement = ev.target;
+    selectedElement.style.outline = "2px solid green";
+    target = ev.target;
+    target.style.filter = "blur(1px) grayscale(100%)";
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+    actionHistory.push({
+      target,
+      action: "move",
+      value: { left: ev.target.originalLeft, top: ev.target.originalTop }
+    });
+  }
 }
 
 const handleMouseLeave = ev => {
-  ev.target.style.outline = "none";
+  if (ev.target.style.outline === "black solid 1px") {
+    ev.target.style.outline = "none";
+  }
   ev.target.removeEventListener("mouseleave", handleMouseLeave);
 }
 
 const handleMouseOver = ev => {
-  ev.target.style.outline = "1px solid black";
+  if (ev.target.style.outline !== "green solid 2px") {
+    ev.target.style.outline = "1px solid black";
+  }
   ev.target.addEventListener("mouseleave", handleMouseLeave);
 }
 
 const handleDblClick = ev => {
   ev.stopPropagation();
-  console.log("%cdblclick", "background:deepskyblue")
   ev.target.contentEditable = true;
 }
 
@@ -96,7 +100,6 @@ const enableTweaklet = () => {
     e.addEventListener("mousedown", handleMouseDown);
     e.addEventListener("mouseover", handleMouseOver);
   });  
-  console.log("%cTweaklet enabled", "color:green;background:lightgray;border:1px solid black;");
 }
 
 const disableTweaklet = () => {
@@ -142,11 +145,13 @@ document.body.appendChild(instructions);
 instructions.addEventListener("mouseover", () => {
   const overlay = document.createElement("div");
   overlay.style = "position:fixed;top:20px;left:55%;background:whitesmoke;width:400px;border:1px solid black;z-index: 100;padding:20px;";
-  overlay.innerHTML = `- Drag any element in the page to change its position<br>
-  - Double click to edit text<br>
-  - Press Shift while using the mouse wheel to resize the element<br>
-  - Move the pointer over an element and use the mouse wheel to bring elements forwards/backwards (change z-index)<br>
-  - Use Ctrl+Z to undo changes<br>
+  overlay.innerHTML = `- Click any element to select<br>
+  - Drag any element in the page to change its position<br>
+  - Double click: edit text<br>
+  - Shift + mouse wheel: resize the element<br>
+  - Mouse wheel: bring element forwards/backwards (change z-index)<br>
+  - Supr: delete element<br>
+  - Ctrl+Z: to undo changes<br>
   `;
   document.body.appendChild(overlay);
   instructions.addEventListener("mouseleave", () => {
@@ -155,7 +160,6 @@ instructions.addEventListener("mouseover", () => {
 });
 
 document.addEventListener("keydown", ev => {
-  console.log("ev", ev.key)
   if (ev.key === "z" && ev.ctrlKey && actionHistory.length > 0) { // Ctrl+Z event
     const lastChange = actionHistory.pop();
     switch (lastChange.action) {
@@ -170,66 +174,76 @@ document.addEventListener("keydown", ev => {
       break;
       case 'z-index':
         lastChange.target.style.zIndex = lastChange.value;
-      break
+      break;
+      case 'remove':
+        lastChange.parent.appendChild(lastChange.target);
+      break;
     }
   } else if (ev.key === "Enter" && ev.ctrlKey) { // Ctrl+Enter event
     ev.target.contentEditable = false;
+  } else if (ev.key === "Delete") {
+    actionHistory.push({
+      target: selectedElement,
+      action: "remove",
+      parent: selectedElement.parentNode
+    });
+    selectedElement.remove();
+    selectedElement = null;
+  } else if (ev.key === "Escape") {
+    selectedElement.style.outline = "none";
+    selectedElement = null;
   } else if (ev.key !== "Shift" && ev.key !== "Control") {
     actionHistory.push({
       target: ev.target,
       action: "edit",
       value: ev.target.innerHTML
     });
-    console.log("actionHistory", actionHistory)
   }
 });
 
 document.addEventListener("wheel", ev => {
-  ev.stopPropagation();
-  console.log("ev", ev, "zIndex", ev.target.style.zIndex);
-  if (ev.deltaY > 0 && ev.shiftKey) { // scale
-    console.log("transform", ev.target.style.transform)
-    if (ev.target.style.transform.includes(`scale(${scale})`)) {
-      ev.target.style.transform = ev.target.style.transform.replace(`scale(${scale})`,`scale(${scale-0.1})`); 
-    } else {
-      ev.target.style.transform += `scale(${scale-0.1})`;
+  if (selectedElement) {
+    ev.stopPropagation();
+    if (ev.deltaY > 0 && ev.shiftKey) { // scale
+      if (selectedElement.style.transform.includes(`scale(${scale})`)) {
+        selectedElement.style.transform = selectedElement.style.transform.replace(`scale(${scale})`,`scale(${scale-0.1})`); 
+      } else {
+        selectedElement.style.transform += `scale(${scale-0.1})`;
+      }
+      scale -= 0.1;
+      actionHistory.push({
+        target: selectedElement,
+        action: "scale",
+        value: selectedElement.style.transform
+      });
+    } else if (ev.deltaY < 0 && ev.shiftKey) {
+        if (selectedElement.style.transform.includes(`scale(${scale})`)) {
+        selectedElement.style.transform = selectedElement.style.transform.replace(`scale(${scale})`,`scale(${scale+0.1})`); 
+      } else {
+        selectedElement.style.transform += `scale(${scale+0.1})`;
+      }
+      scale += 0.1;
+      actionHistory.push({
+        target: selectedElement,
+        action: "scale",
+        value: selectedElement.style.transform
+      });
+    } else if (ev.deltaY > 0) { // move forward/backwards
+      selectedElement.style.zIndex = String(Number(selectedElement.style.zIndex) + 1);
+      actionHistory.push({
+        target: selectedElement,
+        action: "z-index",
+        value: selectedElement.style.zIndex
+      });
+    } else if (ev.deltaY < 0) {
+      selectedElement.style.zIndex = Number(selectedElement.style.zIndex) >= 0 ? String(Number(selectedElement.style.zIndex) - 1) : "0";
+      actionHistory.push({
+        target: selectedElement,
+        action: "z-index",
+        value: selectedElement.style.zIndex
+      });
     }
-    console.log("transform after", ev.target.style.transform)
-    scale -= 0.1;
-    actionHistory.push({
-      target: ev.target,
-      action: "scale",
-      value: ev.target.style.transform
-    });
-  } else if (ev.deltaY < 0 && ev.shiftKey) {
-      if (ev.target.style.transform.includes(`scale(${scale})`)) {
-      ev.target.style.transform = ev.target.style.transform.replace(`scale(${scale})`,`scale(${scale+0.1})`); 
-    } else {
-      ev.target.style.transform += `scale(${scale+0.1})`;
-    }
-    console.log("transform after", ev.target.style.transform)
-    scale += 0.1;
-    actionHistory.push({
-      target: ev.target,
-      action: "scale",
-      value: ev.target.style.transform
-    });
-  } else if (ev.deltaY > 0) { // move forward/backwards
-    ev.target.style.zIndex = String(Number(ev.target.style.zIndex) + 1);
-    actionHistory.push({
-      target: ev.target,
-      action: "z-index",
-      value: ev.target.style.zIndex
-    });
-  } else if (ev.deltaY < 0) {
-    ev.target.style.zIndex = Number(ev.target.style.zIndex) >= 0 ? String(Number(ev.target.style.zIndex) - 1) : "0";
-    actionHistory.push({
-      target: ev.target,
-      action: "z-index",
-      value: ev.target.style.zIndex
-    });
   }
-  console.log("zIndex", ev.target.style.zIndex)
 });
 
 enableTweaklet();
